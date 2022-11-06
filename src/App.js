3402b59556e700bc9651c3c794b3d83b5fae3462
Rastko Kisjelica <kisjelicarastko@gmail.com>
2022-11-06 16:35:23 +0100

    Added navbar and button components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
